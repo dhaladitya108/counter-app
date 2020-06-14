944940f374a25c200e51099f301774dae5cbdb6c
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2020-06-14 21:35:29 +0530

    💫 added functionality (the logic part)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'counter-app';
+
+  count: number = 0;
+
+  handlerIncrease() {
+    if (this.count < 10) this.count += 1;
+  }
+
+  handlerDecrease() {
+    if (this.count > 0) this.count -= 1;
+  }
+
+  handlerReset() {
+    this.count = 0;
+  }
 }
